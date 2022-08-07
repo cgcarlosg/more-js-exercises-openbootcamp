8fcb7c0f8295227d269cb958ebb4fa14de74db29
Carlos Albenio Gutierrez Monroy <carlos.gutierrez01@globant.com>
2022-08-07 18:33:07 -0500

    localstorage, sessionStorage and cookies

---
+++ persistencia/index.js
@@ -0,0 +1,17 @@
+//localStorage.clear();
+
+//localStorage.setItem("nombre", "carlos");
+//localStorage.setItem("apellido", "gutierrez");
+
+//localStorage.setItem("persona", JSON.stringify({nombre:"carlos", apellido :"gutierrez"}))
+
+//console.log(JSON.parse(localStorage.getItem("persona")));
+
+//localStorage.removeItem('nombre');
+
+//sessionStorage.setItem("nombre-sesion",(localStorage.getItem("persona")));
+//
+//document.cookie = "nombreCookie=estudiando";
+//document.cookie = "nombreCaduca=localStorage.getItem('persona') expires=" + new Date().setTime(new Date().getTime()+2000);
+
+//console.log(document.cookie);
